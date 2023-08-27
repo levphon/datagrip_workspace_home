ed8ddc28f2611e1249b65409b91ebd7c24c0a4f8
levphon <liuyufeng@outlook.com>
2023-08-27 23:02:36 +0800

    建库脚本

--- 建库脚本.sql
+++ 建库脚本.sql
@@ -1,3 +1,13 @@
 create database sxgdb default charset utf8mb4 collate utf8mb4_general_ci;
 
 use xxx;
+
+
+create user nacos@'172.17.46.54' identified by 'nacos2023)';
+
+GRANT ALL PRIVILEGES ON nacos_config.* TO nacos@'172.17.46.54' IDENTIFIED BY 'nacos2023)';
+
+# 高版本
+grant all privileges on nacos_config.* to 'nacos'@'172.17.46.54' with grant option;
+
+flush privileges;
